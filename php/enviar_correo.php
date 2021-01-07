f6468e9eb3c9d910ec199557be263d029fca1321
Exequiel Marichal <exemarichal@gmail.com>
2021-01-07 19:59:23 -0300

    cambio en php

---
+++ php/enviar_correo.php
@@ -0,0 +1,24 @@
+<?php
+$nombre = $_POST['nombre'];
+$correo_electronico= $_POST['staticEmail'];
+$opinion= $_POST['exampleFormControlTextarea1'];
+
+
+echo $_POST['nombre'];
+//echo $nombre . " " . $correo_electronico . " " . $opinion;
+
+$mensaje = "Este mensaje fue enviado por " . $nombre . " \r\n";
+$mensaje .= "Su e-mail es: " . $correo_electronico . " \r\n";
+$mensaje .="Mensaje: ".$_POST['opinion'] . " \r\n";
+$mensaje .= "Enviado el " . date('d/m/Y', time());
+
+$headers = "From: " . $correo_electronico;
+
+$para = "appsolucionet@gmail.com";
+$asunto = "Contacto desde la web";
+
+mail($para, $asunto, utf8_decode($mensaje), $headers);
+
+echo 'Mensaje enviado correctamente';
+
+?> 
